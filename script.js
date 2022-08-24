7ed7b6a7b3cdc942c667719016dcfdb0ff337233
GitShaAli <shafeeqalibn@outlook.com>
2022-08-24 20:17:23 +0530

    Commit 3

--- script.js
+++ script.js
@@ -10,23 +10,39 @@ parentContainer.addEventListener('click',(e)=>{
         const imageUrl=document.querySelector(`#${id} img`).src;
         const price=e.target.parentNode.firstElementChild.innerText;
 
+
+        let total = parseFloat(document.getElementById('total').innerText);
+        total+=parseFloat(price);
+        document.getElementById('total').innerText=total;
+
         const add_item = document.createElement('div');
         add_item.classList.add('item');
         add_item.setAttribute('id',`item-${id}`);
 
+        const cart_body = document.getElementById('cart-body');
         add_item.innerHTML = `<span class='cart-items'>
+                               
                                <img class='cart-img' src="${imageUrl}" alt="images">
-                               Name: <span>${name}</span>
+                               <span style="color:red;">${name}</span>
                                
                                Price: <span class='cart-price'>${price}</span>
-                               </span>`
-        cart.appendChild(add_item);
+                               
+                              
+                              <button>Remove</button>
+                              </span>`
+        cart_body.appendChild(add_item);
 
         const container = document.getElementById('notification-container');
         const notif = document.createElement('div');
         notif.classList.add('toast');
         notif.innerHTML = `<h3><strong style="color:red;">${name}</strong> is added to the cart<h3>`;
         container.appendChild(notif);
+
+
+        let count = parseInt(document.querySelector('.count').innerText);
+        count+=1;
+        document.querySelector('.count').innerText=count;
+
         setTimeout(()=>{
             notif.remove();
         },2500)
@@ -40,5 +56,13 @@ parentContainer.addEventListener('click',(e)=>{
     if(e.target.className=='hide'){
         cart.style = "display:none;";
     }
+    if (e.target.innerText=='Remove'){
+        let cartPrice = document.querySelector('#total').innerText;
+        cartPrice = parseFloat(cartPrice).toFixed(2) - parseFloat(document.querySelector(`#${e.target.parentNode.parentNode.id} .cart-price`).innerText).toFixed(2) ;
+        document.querySelector('.count').innerText = parseInt(document.querySelector('.count').innerText)-1
+        document.querySelector('#total').innerText = `${cartPrice.toFixed(2)}`
+        e.target.parentNode.parentNode.remove()
+    }
+
 })
 }
