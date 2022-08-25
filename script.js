ecdc71a46cf2f562091558c63b1b429de0bdbc31
GitShaAli <shafeeqalibn@outlook.com>
2022-08-25 12:02:29 +0530

    Task12

--- script.js
+++ script.js
@@ -4,54 +4,31 @@ const cart = document.getElementById('cart');
 const parentContainer = document.getElementById('ecom');
 
 parentContainer.addEventListener('click',(e)=>{
-    if(e.target.className=='shop-button'){
-        const id = e.target.parentNode.parentNode.id;
-        const name=document.querySelector(`#${id} h4`).innerText;
-        const imageUrl=document.querySelector(`#${id} img`).src;
-        const price=e.target.parentNode.firstElementChild.innerText;
-
-
-        let total = parseFloat(document.getElementById('total').innerText);
-        total+=parseFloat(price);
-        document.getElementById('total').innerText=total;
-
-        const add_item = document.createElement('div');
-        add_item.classList.add('item');
-        add_item.setAttribute('id',`item-${id}`);
-
-        const cart_body = document.getElementById('cart-body');
-        add_item.innerHTML = `<span class='cart-items'>
-                               
-                               <img class='cart-img' src="${imageUrl}" alt="images">
-                               <span style="color:red;">${name}</span>
-                               
-                               Price: <span class='cart-price'>${price}</span>
-                               
-                              
-                              <button>Remove</button>
-                              </span>`
-        cart_body.appendChild(add_item);
-
-        const container = document.getElementById('notification-container');
-        const notif = document.createElement('div');
-        notif.classList.add('toast');
-        notif.innerHTML = `<h3><strong style="color:red;">${name}</strong> is added to the cart<h3>`;
-        container.appendChild(notif);
-
+    
+    if (e.target.className=='addCart-button'){
+        const productId = Number(e.target.parentNode.parentNode.id.split('-')[1]);
+        console.log(productId)
+        axios.post('http://localhost:3000/cart', { productId: productId})
+        .then(response => {
+            if(response.status==200){
+                notification(response.data.message);
+                // console.log("Quant :"+response.data.quantity);
+            }
+            else    throw new Error('Unable to add product');
+        })
+        .catch(err => {
+           notification(err, true);
+        });
 
         let count = parseInt(document.querySelector('.count').innerText);
         count+=1;
         document.querySelector('.count').innerText=count;
 
-        setTimeout(()=>{
-            notif.remove();
-        },2500)
-
     }
 
-
+    
     if(e.target.className=='see-cart' || e.target.className=='navCart'){
-        cart.style = "display:block;";
+            cart.style = "display:block;";    
     }
     if(e.target.className=='hide'){
         cart.style = "display:none;";
@@ -63,15 +40,28 @@ parentContainer.addEventListener('click',(e)=>{
         document.querySelector('#total').innerText = `${cartPrice.toFixed(2)}`
         e.target.parentNode.parentNode.remove()
     }
+    if(e.target.className=='see-cart' || e.target.className=='navCart'){
+        axios.get('http://localhost:3000/cart').then(products => {
+        for(let i=0;i<products.data.length;i++){
+            const id = products.data[i].id;
+            const name=products.data[i].name;
+            const imageUrl=products.data[i].imageUrl;
+            const price=products.data[i].price;
+            let total = parseFloat(document.getElementById('total').innerText);
+            total+=parseFloat(price);
+            document.getElementById('total').innerText=total;
+            
+            showCart(id,name,imageUrl,price);
+            
+        }
+    })
+    }
 
 })
 }
 
 
 const parentNode = document.getElementById('section-container');
-if(parentNode)console.log('yes');
-else{console.log("no")}
-// console.log(parents)
 window.addEventListener('DOMContentLoaded',()=>{
     const parentNode = document.getElementById('section-container');
     axios.get('http://localhost:3000/products').then((products) => {
@@ -86,10 +76,49 @@ window.addEventListener('DOMContentLoaded',()=>{
                      </div>
                                      <div class="prod-details">
                          <span>${products.data[i].price}</span>
-                         <button class="shop-button" type='button'>ADD TO CART</button>
+                         <button class="addCart-button" type='button'>ADD TO CART</button>
                     </div>
                 </div>`
                 parentNode.innerHTML += productHtml
         }
     })
+
+    axios.get('http://localhost:3000/cart').then(products => {
+        const cartCount = products.data.length;
+        document.querySelector('.count').innerText=cartCount;
+    })
+
+
 })
+
+
+function showCart(id,name,imageUrl,price){
+    const add_item = document.createElement('div');
+            add_item.classList.add('item');
+            add_item.setAttribute('id',`item-${id}`);
+    
+            const cart_body = document.getElementById('cart-body');
+            add_item.innerHTML = `<span class='cart-items'>
+                                   
+                                   <img class='cart-img' src="${imageUrl}" alt="images">
+                                   <span style="color:red;">${name}</span>
+                                   
+                                   Price: <span class='cart-price'>${price}</span>
+                                   
+                                  
+                                  <button>Remove</button>
+                                  </span>`
+            cart_body.appendChild(add_item);
+}
+
+function notification(res){
+        const container = document.getElementById('notification-container');
+        const notif = document.createElement('div');
+        notif.classList.add('toast');
+        notif.innerHTML = `<h3><strong style="color:red;">${res}</strong> :)<h3>`;
+        container.appendChild(notif);
+
+        setTimeout(()=>{
+            notif.remove();
+        },2500)
+}
